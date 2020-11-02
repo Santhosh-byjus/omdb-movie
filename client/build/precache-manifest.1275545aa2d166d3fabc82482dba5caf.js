b8a27da160e3f9bc48fcf23df97ec9637154e6a4
santhosh.a1@byjus.com <santhosh.a1@byjus.com>
2020-11-02 11:11:27 +0530

    added build

---
+++ client/build/precache-manifest.1275545aa2d166d3fabc82482dba5caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ce6dedf64bafcc78fb691b6cb05f3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "549043a2930a780ea091",
+    "url": "/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "4df0bef13e2fb56b8e2e",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "549043a2930a780ea091",
+    "url": "/static/js/2.9591b665.chunk.js"
+  },
+  {
+    "revision": "acb6172f4194c891db1f3ade83d63cfc",
+    "url": "/static/js/2.9591b665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df0bef13e2fb56b8e2e",
+    "url": "/static/js/main.fca16835.chunk.js"
+  },
+  {
+    "revision": "94d046c0232f5571ca3e",
+    "url": "/static/js/runtime-main.eef8ae88.js"
+  }
+]);
